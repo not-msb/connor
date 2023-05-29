3f01d23d887b3d6ad0f4c039b224f6caa95c8045
MSB246 <msembulut@gmail.com>
2023-05-29 13:59:06 +0200

    Ran 'cargo fmt' on code

--- src/ast.rs
+++ src/ast.rs
@@ -1,45 +1,53 @@
-use crate::types::{Ast, Type, BinOp, Storage, Env};
+use crate::types::{Ast, BinOp, Env, Storage, Type};
 
-use nom::character::complete::{
-    char,
-    digit1,
-    alpha1,
-    alphanumeric0,
-    multispace0,
-    multispace1,
-    one_of,
-};
-use nom::bytes::complete::{
-    tag,
-    take_until,
-};
 use nom::branch::alt;
-use nom::sequence::tuple;
+use nom::bytes::complete::{tag, take_until};
+use nom::character::complete::{
+    alpha1, alphanumeric0, char, digit1, multispace0, multispace1, one_of,
+};
 use nom::multi::many0;
+use nom::sequence::tuple;
 use nom::IResult;
 
 impl Ast {
     fn lit_var(input: &str) -> IResult<&str, Ast> {
         let (input, ident) = Ast::identifier(input)?;
-        Ok((input, Ast::Literal { ty: Type::Var, storage: Storage::Stack, content: ident }))
+        Ok((
+            input,
+            Ast::Literal {
+                ty: Type::Var,
+                storage: Storage::Stack,
+                content: ident,
+            },
+        ))
     }
 
     fn lit_u32(input: &str) -> IResult<&str, Ast> {
         let (input, num) = digit1(input)?;
-        Ok((input, Ast::Literal { ty: Type::U32, storage: Storage::Direct, content: num.to_string() }))
+        Ok((
+            input,
+            Ast::Literal {
+                ty: Type::U32,
+                storage: Storage::Direct,
+                content: num.to_string(),
+            },
+        ))
     }
 
     fn lit_bool(input: &str) -> IResult<&str, Ast> {
         let (input, cond) = alt((tag("true"), tag("false")))(input)?;
-        Ok((input, Ast::Literal { ty: Type::Bool, storage: Storage::Direct, content: cond.to_string() }))
+        Ok((
+            input,
+            Ast::Literal {
+                ty: Type::Bool,
+                storage: Storage::Direct,
+                content: cond.to_string(),
+            },
+        ))
     }
 
     fn literal(input: &str) -> IResult<&str, Ast> {
-        alt((
-            Ast::lit_u32,
-            Ast::lit_bool,
-            Ast::lit_var,
-        ))(input)
+        alt((Ast::lit_u32, Ast::lit_bool, Ast::lit_var))(input)
     }
 
     fn ty_u32(input: &str) -> IResult<&str, Type> {
@@ -58,11 +66,7 @@ impl Ast {
     }
 
     fn ty(input: &str) -> IResult<&str, Type> {
-        alt((
-            Ast::ty_u32,
-            Ast::ty_bool,
-            Ast::ty_void,
-        ))(input)
+        alt((Ast::ty_u32, Ast::ty_bool, Ast::ty_void))(input)
     }
 
     fn identifier(input: &str) -> IResult<&str, String> {
@@ -84,37 +88,43 @@ impl Ast {
             '-' => BinOp::Sub,
             '*' => BinOp::Mul,
             '/' => BinOp::Div,
-            _ => unreachable!()
+            _ => unreachable!(),
         };
 
-        Ok((input, Ast::BinOp { bin_op, left: Box::new(left), right: Box::new(right) }))
+        Ok((
+            input,
+            Ast::BinOp {
+                bin_op,
+                left: Box::new(left),
+                right: Box::new(right),
+            },
+        ))
     }
 
     fn block(input: &str) -> IResult<&str, Ast> {
         let (input, (_, _, nodes, _)) = tuple((
-                char('{'),
+            char('{'),
+            multispace0,
+            many0(tuple((
+                alt((Ast::assignment, Ast::expr)),
                 multispace0,
-                many0(tuple((
-                    alt((
-                        Ast::assignment,
-                        Ast::expr,
-                    )),
-                    multispace0,
-                    char(';'),
-                    multispace0,
-                ))),
-                char('}'),
+                char(';'),
+                multispace0,
+            ))),
+            char('}'),
         ))(input)?;
 
-        Ok((input, Ast::Block { ty: Type::Void, nodes: nodes.into_iter().map(|(ast, _, _, _)| ast).collect() }))
+        Ok((
+            input,
+            Ast::Block {
+                ty: Type::Void,
+                nodes: nodes.into_iter().map(|(ast, _, _, _)| ast).collect(),
+            },
+        ))
     }
 
     fn expr(input: &str) -> IResult<&str, Ast> {
-        alt((
-            Ast::bin_op,
-            Ast::literal,
-            Ast::block,
-        ))(input)
+        alt((Ast::bin_op, Ast::literal, Ast::block))(input)
     }
 
     fn assignment(input: &str) -> IResult<&str, Ast> {
@@ -128,7 +138,14 @@ impl Ast {
             Ast::expr,
         ))(input)?;
 
-        Ok((input, Ast::Assign { ty, variable: identifier, expr: Box::new(expr) }))
+        Ok((
+            input,
+            Ast::Assign {
+                ty,
+                variable: identifier,
+                expr: Box::new(expr),
+            },
+        ))
     }
 
     pub fn check_type(&self, env: &mut Env) -> Type {
@@ -139,12 +156,23 @@ impl Ast {
                     node.check_type(&mut scope);
                 }
                 *ty
-            },
-            Ast::Literal { ty, storage: _, content } => match ty {
-                Type::Var => *env.types.get(content).unwrap_or_else(|| panic!("Variable {content:?}'s type undefined")),
+            }
+            Ast::Literal {
+                ty,
+                storage: _,
+                content,
+            } => match ty {
+                Type::Var => *env
+                    .types
+                    .get(content)
+                    .unwrap_or_else(|| panic!("Variable {content:?}'s type undefined")),
                 ty => *ty,
             },
-            Ast::BinOp { bin_op: _, left, right } => {
+            Ast::BinOp {
+                bin_op: _,
+                left,
+                right,
+            } => {
                 assert_eq!(left.check_type(env), right.check_type(env));
 
                 left.check_type(env)
@@ -156,16 +184,12 @@ impl Ast {
                 assert_eq!(*ty, exty);
 
                 *ty
-            },
+            }
         }
     }
 
     fn comment(input: &str) -> IResult<&str, &str> {
-        let (input, (_, body, _)) = tuple((
-            tag("//"),
-            take_until("\n"),
-            tag("\n"),
-        ))(input)?;
+        let (input, (_, body, _)) = tuple((tag("//"), take_until("\n"), tag("\n")))(input)?;
 
         Ok((input, body))
     }
@@ -180,14 +204,8 @@ impl Ast {
                 continue;
             }
 
-            let (output, (ast, _, _sq)) = tuple((
-                alt((
-                    Ast::assignment,
-                    Ast::expr,
-                )),
-                multispace0,
-                char(';'),
-            ))(input)?;
+            let (output, (ast, _, _sq)) =
+                tuple((alt((Ast::assignment, Ast::expr)), multispace0, char(';')))(input)?;
 
             input = output.trim_start();
             nodes.push(ast);
@@ -205,21 +223,43 @@ impl Ast {
                 for node in nodes {
                     output += &node.assemble(&mut scope);
                 }
-            },
-            Ast::Literal { ty, storage, content } => {
+            }
+            Ast::Literal {
+                ty,
+                storage,
+                content,
+            } => {
                 output += &match ty {
-                    Type::Var => match env.types.get(content).unwrap_or_else(|| panic!("Variable {content:?}'s type undefined")) {
+                    Type::Var => match env
+                        .types
+                        .get(content)
+                        .unwrap_or_else(|| panic!("Variable {content:?}'s type undefined"))
+                    {
                         Type::Var => unreachable!("Var type cannot contain another Var"),
                         Type::Void => unimplemented!("Should've been removed by optimizer"),
-                        Type::U32 => format!("mov eax, dword[ebp-{}]\n", env.stack.get(content).unwrap_or_else(|| panic!("Variable {content:?}'s stack location undefined"))),
-                        Type::Bool => format!("movsx eax, byte[ebp-{}]\n", env.stack.get(content).unwrap_or_else(|| panic!("Variable {content:?}'s stack location undefined"))),
+                        Type::U32 => format!(
+                            "mov eax, dword[ebp-{}]\n",
+                            env.stack.get(content).unwrap_or_else(|| panic!(
+                                "Variable {content:?}'s stack location undefined"
+                            ))
+                        ),
+                        Type::Bool => format!(
+                            "movsx eax, byte[ebp-{}]\n",
+                            env.stack.get(content).unwrap_or_else(|| panic!(
+                                "Variable {content:?}'s stack location undefined"
+                            ))
+                        ),
                     },
                     Type::Void => unimplemented!("Should've been removed by optimizer"),
                     Type::U32 => format!("mov eax, {content}\n"),
                     Type::Bool => format!("mov eax, {content}\n"),
                 };
-            },
-            Ast::BinOp { bin_op, left, right } => {
+            }
+            Ast::BinOp {
+                bin_op,
+                left,
+                right,
+            } => {
                 output += &left.assemble(env);
                 output += "mov edi, eax\n";
                 output += &right.assemble(env);
@@ -229,24 +269,24 @@ impl Ast {
                     BinOp::Add => {
                         output += "add edi, esi\n";
                         output += "mov eax, edi\n";
-                    },
+                    }
                     BinOp::Sub => {
                         output += "sub edi, esi\n";
                         output += "mov eax, edi\n";
-                    },
+                    }
                     BinOp::Mul => {
                         output += "mov eax, edi\n";
                         output += "mov edx, esi\n";
                         output += "mul edx\n";
-                    },
+                    }
                     BinOp::Div => {
                         output += "xor edx, edx\n";
                         output += "mov eax, edi\n";
                         output += "mov edx, esi\n";
                         output += "div edx\n";
-                    },
+                    }
                 }
-            },
+            }
             Ast::Assign { ty, variable, expr } => {
                 env.stack.insert(variable.to_owned(), env.ssize);
                 env.ssize += ty.size();
@@ -254,7 +294,7 @@ impl Ast {
 
                 output += &expr.assemble(env);
                 output += &format!("mov [ebp-{}], eax\n", env.stack.get(variable).unwrap());
-            },
+            }
         }
 
         output
